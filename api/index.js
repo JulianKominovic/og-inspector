f0ec3cb0966cdfd6543a401abc1bd2ab4f99d5e5
Julian Kominovic <juliankominovic@gmail.com>
2022-09-02 16:47:37 -0300

    #INIT

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const app = require("express")();
+const ogs = require("open-graph-scraper-lite");
+const cors = require("cors");
+
+app.use(json());
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  ogs({
+    url: req.body?.url,
+  })
+    .then((r) => res.send(r))
+    .catch((err) => res.send(err));
+});
+
+module.exports = app;
