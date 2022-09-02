c4150d8a619f3108146181d1fe27704d313ee9e5
Julian Kominovic <juliankominovic@gmail.com>
2022-09-02 17:06:38 -0300

    #FEAT: vercel serverless function

--- api/index.js
+++ api/index.js
@@ -1,16 +1,9 @@
-const app = require("express")();
 const ogs = require("open-graph-scraper-lite");
-const cors = require("cors");
 
-app.use(json());
-app.use(cors());
-
-app.post("/api", (req, res) => {
+export default function og(req, res) {
   ogs({
     url: req.body?.url,
   })
-    .then((r) => res.send(r))
-    .catch((err) => res.send(err));
-});
-
-module.exports = app;
+    .then((r) => res.status(200).send(r))
+    .catch((err) => res.status(500).send(err));
+}
